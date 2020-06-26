3e864d5657b287fb07c45743b7c361d0c5ff4c14
bartoszjarzab <bartosz.jarzab@gmail.com>
2020-06-26 14:44:46 +0200

    Initial commit

---
+++ src/main/java/com/springframework/domain/Category.java
@@ -0,0 +1,17 @@
+package com.springframework.domain;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+@Data
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+}
