fb6e6ab7dcc8ee050c7f333e37bf51fb98a2e4c6
bartoszjarzab <bartosz.jarzab@gmail.com>
2020-07-01 08:55:51 +0200

    added swagger

--- src/main/java/com/springframework/controllers/v1/CustomerController.java
+++ src/main/java/com/springframework/controllers/v1/CustomerController.java
@@ -3,6 +3,7 @@ package com.springframework.controllers.v1;
 import com.springframework.api.v1.model.CustomerDTO;
 import com.springframework.api.v1.model.CustomerListDTO;
 import com.springframework.services.CustomerService;
+import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
